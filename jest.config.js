bc01bc18a2f713923a5dc026786763081527589c
Rumbo <engineerr0528@gmail.com>
2023-01-05 08:12:21 -0800

    add babel, jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
       '<rootDir>/__mocks__/fileMock.ts',
+    "\\.(css|scss)$": "<rootDir>/__mocks__/fileMock.ts",
+    "axios": "<rootDir>/node_modules/axios/dist/node/axios.cjs",
   },
-  setupFilesAfterEnv: ['<rootDir>/template/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
